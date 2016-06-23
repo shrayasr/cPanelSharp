f1a8a50f928799488f107763592299134350852c
Shrayas <shrayasr@gmail.com>
2016-06-23 15:18:39 +0530

    Fix nuspec issues
    First package deployed! :)
    https://www.nuget.org/packages/cPanelSharp/1.0.0

--- cPanelSharp/Properties/AssemblyInfo.cs
+++ cPanelSharp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("cPanelSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple C# wrapper for accessing cPanel APIs")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Logic Soft")]
 [assembly: AssemblyProduct("cPanelSharp")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © Logic Soft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
